cca62692c724aef9f1b4db2c120401448b0e03f9
mzappi88 <mzappi@cattler.farm>
2025-10-13 20:32:23 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Microsoft Clarity */}
+        {/* Microsoft Clarity - Multiple Projects */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
+              // Clarity Project 1: nk835onlfr
               (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "nk835onlfr");
+              
+              // Clarity Project 2: nk835onlfr (redundancy)
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity2", "script", "nk835onlfr");
             `,
           }}
         />
